bec9019dda8b679d833d5361bda663a6271989eb
jb <jessb.epicodus@gmail.com>
2022-03-14 15:27:03 -0700

    add status update

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
-  public class Item
-  {
-    public int ItemId { get; set; }
-    public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
-  }
+    public class Item
+    {
+        public Item()
+        {
+            this.JoinEntities = new HashSet<CategoryItem>();
+        }
+
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        // public string Deadline { get; set; }
+        public virtual ICollection<CategoryItem> JoinEntities { get;}
+    }
 }
